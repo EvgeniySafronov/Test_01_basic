2bba3f93dab24f463645a811f9ec2baa44071b25
E.Safronov <madjuster-io@yandex.ru>
2022-07-24 19:39:49 +1000

    add printing word <= 3 symbols

--- Program.cs
+++ Program.cs
@@ -7,3 +7,14 @@ for (int i = 0; i < array.Length; i++)
     Console.Write(array[n] + " ");
     n++;
 }
+
+for (int i = 0; i < array.Length; i++)
+{
+    int n = 0;
+    if (array[i].Length <=3)
+    {
+        array[n] = array[i];
+        Console.WriteLine(array[n]);
+        n++;
+    }
+}
