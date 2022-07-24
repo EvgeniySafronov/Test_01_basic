bd5d6acf688ce0dde98b42506ed012eb60341066
E.Safronov <madjuster-io@yandex.ru>
2022-07-24 19:34:53 +1000

    add printing an array

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string[] array = { "Hello", "Cat", "Russia", "Case", "Bat", "Mickey" };
+Console.WriteLine("Изначальный заданный массив:");
+for (int i = 0; i < array.Length; i++)
+{
+    int n = 0;
+     array[n] = array[i];
+    Console.Write(array[n] + " ");
+    n++;
+}
