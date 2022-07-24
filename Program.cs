ceb2a5ef48ad81252706913daa432682551ea2bb
E.Safronov <madjuster-io@yandex.ru>
2022-07-24 20:18:15 +1000

    добавил печать созданного массива

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
 ﻿string[] array = { "Hello", "Cat", "Russia", "Case", "Bat", "Mickey" };
+for (int i = 0; i < array.Length; i++)
+{
+    int n = 0;
+     array[n] = array[i];
+    Console.Write(array[n] + " ");
+    n++;
+}
