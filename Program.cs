010d494c9adcad1852ed2132a43e5a437896965b
E.Safronov <madjuster-io@yandex.ru>
2022-07-24 20:18:57 +1000

    добавил печать слов длина которых <= трех символов

--- Program.cs
+++ Program.cs
@@ -6,3 +6,13 @@ for (int i = 0; i < array.Length; i++)
     Console.Write(array[n] + " ");
     n++;
 }
+for (int i = 0; i < array.Length; i++)
+{
+    int n = 0;
+    if (array[i].Length <=3)
+    {
+        array[n] = array[i];
+        Console.WriteLine(array[n]);
+        n++;
+    }
+}
