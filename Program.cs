731e440d018c2896229508980404ad54c0c9720e
E.Safronov <madjuster-io@yandex.ru>
2022-07-24 20:20:09 +1000

    добавил некоторые пояснения к печати массива\слов из массива

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿string[] array = { "Hello", "Cat", "Russia", "Case", "Bat", "Mickey" };
+Console.WriteLine("Изначальный заданный массив:");
 for (int i = 0; i < array.Length; i++)
 {
     int n = 0;
@@ -6,6 +7,8 @@ for (int i = 0; i < array.Length; i++)
     Console.Write(array[n] + " ");
     n++;
 }
+Console.WriteLine(" ");
+Console.WriteLine("Слова в данном массиве длинна которых не больше трех символов: ");
 for (int i = 0; i < array.Length; i++)
 {
     int n = 0;
